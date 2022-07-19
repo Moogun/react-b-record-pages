637c4cf8ac24b3569933548fa7e29e350242f474
Moogun <moogunjung@gmail.com>
2022-07-19 16:47:47 +0900

    2 files modified

--- src/pages/league/leagueEdit.js
+++ src/pages/league/leagueEdit.js
@@ -13,7 +13,7 @@ import { PageHeader } from '../../pageHeader.js';
 export default function LeagueEdit({}) {
   let location = useLocation();
   let leagueToEdit = location.state;
-  console.log('leagueToEdit', leagueToEdit);
+  console.log('[leagueToEdit]', leagueToEdit);
 
   const {tokens} = useTheme()
 
@@ -26,13 +26,13 @@ export default function LeagueEdit({}) {
       >
         <Card column="1/3" backgroundColor={tokens.colors.blue[10]}>
           <nav>
-            <Link to="info" style={{fontSize: "13px"}}>기본정보</Link>
+            <Link to="info" state={leagueToEdit} style={{fontSize: "13px"}}>기본정보</Link>
             <br />
-            <Link to="participants" style={{fontSize: "13px"}}>참가팀</Link>
+            <Link to="participants"  state={leagueToEdit} style={{fontSize: "13px"}}>참가팀</Link>
             <br />
-            <Link to="schedules" style={{fontSize: "13px"}}>경기일정</Link>
+            <Link to="schedules" state={leagueToEdit} style={{fontSize: "13px"}}>경기일정</Link>
             <br />
-            <Link to="results" style={{fontSize: "13px"}}>경기기록</Link>
+            <Link to="results" state={leagueToEdit} style={{fontSize: "13px"}}>경기기록</Link>
           </nav>
         </Card>
         <Card column="3/-1" backgroundColor={tokens.colors.green[10]}>
--- src/pages/league/leagueEditInfo.js
+++ src/pages/league/leagueEditInfo.js
@@ -7,14 +7,14 @@ import {
   Link,
   Outlet,
 } from 'react-router-dom';
-import { useTheme, Grid, Card, Button } from '@aws-amplify/ui-react';
+import { useTheme, Grid, Card, Button, Text, View } from '@aws-amplify/ui-react';
 
 export default function LeagueEditInfo({}) {
   let params = useParams();
-  // console.log('leagueToEditInfo params', params); return one of the following, undefined or null  
+  // console.log('leagueToEditInfo params', params); return one of the following, undefined or null
   let location = useLocation();
-    let leagueToEdit = location.state; // return null 
-  // console.log('leagueToEditInfo leagueToEdit', leagueToEdit);
+  let leagueToEdit = location.state; // return null
+  console.log('leagueToEditInfo leagueToEdit', leagueToEdit);
 
   const { tokens } = useTheme();
 
@@ -25,38 +25,57 @@ export default function LeagueEditInfo({}) {
         templateColumns="repeat(8, 1fr)"
         backgroundColor={tokens.colors.red[10]}
       >
-        {getCurrentSubMenu(location.pathname)}
-
+        {getCurrentSubMenu(location.pathname, leagueToEdit)}
       </Grid>
     </div>
   );
 }
 
-function getCurrentSubMenu(path) {
+function getCurrentSubMenu(path, leagueToEdit) {
   if (path.includes('info')) {
-    return <Info />;
+    return <Info column="1/-1" leagueToEdit={leagueToEdit} />;
   } else if (path.includes('participants')) {
-    return  <Participants />;;
+    return <Participants leagueToEdit={leagueToEdit} />;
   } else if (path.includes('schedules')) {
-    return  <Schedules />;
+    return <Schedules column="1/-1" leagueToEdit={leagueToEdit} />;
   } else if (path.includes('results')) {
-    return  <Results />;;
+    return <Results column="1/-1" leagueToEdit={leagueToEdit} />;
   }
   return <div> ''--</div>;
 }
 
-function Info() {
-return <div> info </div>
+function Info({column, leagueToEdit }) {
+  return <View column={column}> info </View>;
 }
 
-function Participants() {
-  return <div> participants </div>
+function Participants({column, leagueToEdit }) {
+  let parti = leagueToEdit.teamsParticipating;
+  return (
+    <View column={column}>
+      <Text> participants </Text>
+      {parti.map((p) => {
+        return <div> {p} </div>;
+      })}
+    </View>
+  );
 }
 
-function Schedules() {
-  return <div> schedules </div>
+function Schedules({column,  leagueToEdit }) {
+  let ga = leagueToEdit.games;
+  console.log('[column]', column)
+  return (
+    <View column={column}>
+      <Text> schedules </Text>
+      {ga.map((g) => {
+        return <div>
+           {/* {g.date} - {g.time}  */}
+           e industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop pu
+           </div>;
+      })}
+    </View>
+  );
 }
 
-function Results() {
-  return <div> results </div>
+function Results({column, leagueToEdit }) {
+  return  <View column={column}> results </View>;
 }
